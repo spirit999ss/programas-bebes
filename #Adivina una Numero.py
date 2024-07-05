18309d9b15b591cd562b7962ffd637c571f573d2
Sandino Bone <sandinobq999@gmail.com>
2024-07-05 16:40:18 -0500

    Adivina un Numero
    Es Sencillo solo te pide que adivines el numero 
    *Si esta mal te dice que es mas alto o mas bajo 
    *Tiene un Bucle para seguir jugando ... solo te pregunta 1 vez si quieres de hay es un Bucle
    *Te pide el Nombre y lo Utiliza(No se porque lo agg xd)
    *

---
+++ #Adivina una Numero.py
@@ -0,0 +1,45 @@
+#Adivina un Numero
+import random 
+print("..............................................")
+print("..............................................")
+print("..............................................")
+print("BIenvenido al Juego Adivina un Numero")
+Nombre = input("Ingrese su Nombre : ")
+def Adivina ():
+    num_r = random.randint(1,100) 
+    while True :
+        try:
+            print("..............................................")
+            num_a=int(input(f"{Nombre} Estoy Pensando un Numero del 1 al 100 ¡ADIVINALO! : "))
+        except ValueError:
+            print (" :( Ingresa un Numero por Favor :(  ")
+            continue
+        if  num_r  > num_a :
+            print("..............................................")
+            print(f"{Nombre} El Numero es mas Alto :) ")
+        elif num_r < num_a :
+            print("..............................................")
+            print(f"{Nombre} El Numero es mas Bajo :) ")     
+        else :
+            print("..............................................")
+            print("..............................................")
+            print("..............................................")
+            print("Adivinaste :) ")
+            print("..............................................")
+            print("..............................................")
+            print("..............................................")
+
+            break 
+
+Adivina()
+#Se Pregunta al Usuario SI Quiere Seguir JUGANDO 
+resultado =input("Presione ENTER para Continuar o Si Quiere Salir Cualquier Caracter : ")             
+while resultado == "" :
+    Adivina ()
+    continue
+while resultado == resultado.lower() :
+    print("GRACIAS :) POR ELEGIRNOS")
+    break
+
+
+    
